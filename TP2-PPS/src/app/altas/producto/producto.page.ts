12646982b35f53af40daa7310e04db253109b36a
Tomi <atienzotomi@gmail.com>
2023-10-30 02:51:00 -0300

    logica de guardado en firebase terminada

--- TP2-PPS/src/app/altas/producto/producto.page.ts
+++ TP2-PPS/src/app/altas/producto/producto.page.ts
@@ -4,6 +4,8 @@ import { Firestore } from '@angular/fire/firestore';
 import { getStorage, ref, uploadString } from 'firebase/storage';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { QrService } from 'src/app/services/qr.service';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-producto',
@@ -11,47 +13,140 @@ import { QrService } from 'src/app/services/qr.service';
   styleUrls: ['./producto.page.scss'],
 })
 export class ProductoPage implements OnInit {
-  json:any;
+  json:any;//contenido qr
+  fotoCapturadas:any[] = [];
+  fotosSubidas:any[] = [];
+  urlQr:any;
+  fotosRestantes = 3;
+  nombreQr = '';
+  nombrePr = '';
 
-  constructor(private qr: QrService, private firestore: Firestore, private angularFirestorage: AngularFireStorage) { }
+  constructor(private auth: AuthService, private qr: QrService, private firestore: Firestore, private angularFirestorage: AngularFireStorage) { }
 
   ngOnInit() 
   {
-    this.json = {};
+    this.json = {probando: true};
+    this.auth.login('algo@algo.com','123456');
   }
 
-  guardarQr()
+  guardar()
   {
-    let url = this.qr.generarCodigoQR(JSON.stringify(this.json));
+    this.urlQr = this.qr.generarCodigoQR(JSON.stringify(this.json));
 
-    if(url)
+    if(this.urlQr)
     {
-      let obj = {nombre: "", descripcion: "", tiempo: 0, precio: 0, fotos: []};
-      this.subirQr(obj, url);
+      let obj = {nombre: "prueba", descripcion: "necesito que ande", tiempo: 100, precio: 100};
+      this.subirTodo(obj, this.urlQr);
     }
   }
 
-  subirQr(obj:any, url:string)
+  subirTodo(obj:any, url:string)
   {
     try
     {
       const fecha = new Date().getTime();
-      const storage = getStorage();
-      const nombre = `productos/qr/${obj.nombre.replace(" ", "_")} ${fecha}`;/////////////CAMBIAR LOS OBJ. POR THIS.
-      const storageRef = ref(storage, nombre);
+      this.nombreQr = `productos/${obj.nombre.replace(" ", "_")}/qr_${fecha}`;/////////////CAMBIAR LOS OBJ. POR THIS.
+      this.nombrePr = `productos/${obj.nombre.replace(" ", "_")}/fotos/1`;/////////////CAMBIAR LOS OBJ. POR THIS.
 
-      uploadString(storageRef as any, url as any, 'data_url').then(()=>{/////////////CAMBIAR LOS OBJ. POR THIS.
-        const urlPromise = this.angularFirestorage.ref(nombre).getDownloadURL().toPromise();
-        urlPromise.then((url2: any) => 
+      this.subirQr(url)?.then(()=>
+      {
+        const promesa = this.angularFirestorage.ref(this.nombreQr).getDownloadURL().toPromise();
+        promesa.then((urlQr)=>
         {
-          let obj_ = {...obj, qr: url2};
-          FirestoreService.guardarFs('productos', obj_, this.firestore)
-        });
+          this.subirFotos(this.fotoCapturadas[0])?.
+          then(()=>
+          {
+            const promesa = this.angularFirestorage.ref(this.nombrePr).getDownloadURL().toPromise();
+            promesa.then((url: any)=>
+            {
+              this.fotosSubidas.push(url);
+              this.nombrePr = `productos/${obj.nombre.replace(" ", "_")}/fotos/2`
+              this.subirFotos(this.fotoCapturadas[1])?.then(()=>
+              {
+                const promesa = this.angularFirestorage.ref(this.nombrePr).getDownloadURL().toPromise();
+                promesa.then((url: any)=>
+                {
+                  this.fotosSubidas.push(url);
+                  this.nombrePr = `productos/${obj.nombre.replace(" ", "_")}/fotos/3`
+                  this.subirFotos(this.fotoCapturadas[2])?.then(()=>
+                  {
+                    const promesa = this.angularFirestorage.ref(this.nombrePr).getDownloadURL().toPromise();
+                    promesa.then((url: any)=>
+                    {
+                      this.fotosSubidas.push(url);
+                      FirestoreService.guardarFs('productos', {...obj, fotos: this.fotosSubidas, qr: urlQr}, this.firestore);
+                      this.auth.mostrarToastExito("¡Alta realizada con éxito!");
+                    });
+                  });
+                });
+              });
+            });
+          });
+        })
       });
     }
     catch
     {
       //this.auth.mostrarToastError("Error al subir la foto...");
+    }
+  }
+
+  async sacarFotos()
+  {
+    try
+    {
+      if(this.fotosRestantes > 0)
+      {
+        let fotoCapturada = await Camera.getPhoto({
+          resultType: CameraResultType.DataUrl,
+          source: CameraSource.Camera,
+          quality: 100,
+          webUseInput: true,
+        });
+  
+        this.fotoCapturadas.push(fotoCapturada.dataUrl);
+        this.fotosRestantes--;
+        console.log(this.fotoCapturadas);
+        this.auth.mostrarToastExito("¡Foto cargada con éxito!")
+      }
+      else
+      {
+        this.auth.mostrarToastError('¡Ya tomó las tres fotos!');
+      }
+    }
+    catch(error)
+    {
+      console.error('error: ' + error);
+    }
+  }
+
+  subirQr(url:string)
+  {
+    try
+    {
+      const storage = getStorage();
+      const storageRefQr = ref(storage, this.nombreQr);
+      return uploadString(storageRefQr as any, url as any, 'data_url');
+    }
+    catch(error)
+    {
+      console.log("error" + error)
+      return;
+    }
+  }
+
+  subirFotos(url:any)
+  {
+    try
+    {
+      const storage = getStorage();
+      const storageRef = ref(storage, this.nombrePr);
+      return uploadString(storageRef as any, url as any, 'data_url');
+    }
+    catch(error)
+    {
+      console.log("error" + error);
+      return;
     }
   }
 }
