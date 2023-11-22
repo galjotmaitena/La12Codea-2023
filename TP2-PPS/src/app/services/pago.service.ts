1b31da16498a0b78b489c03f8a948b1bdf4b9e26
Tomi <atienzotomi@gmail.com>
2023-11-22 12:43:35 -0300

    mp

--- TP2-PPS/src/app/services/pago.service.ts
+++ TP2-PPS/src/app/services/pago.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,10 @@ export class PagoService {
   private baseUrl = 'https://api.mercadopago.com/checkout/preferences';
   private accessToken = 'TEST-1249301994362591-111113-cff2f1acbb2b52546ca0b292da4ab9bb-404691086';
 
-  constructor(private http: HttpClient, private router: Router) 
-  {
+  constructor(private http: HttpClient, private router: Router) {
     this.router.events.pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
       if (event.url.includes('/success') || event.url.includes('/failure') || event.url.includes('/pending')) {
-        this.router.navigate(['/homeClientes'], { skipLocationChange: true });
+        this.manipularHistorial();
       }
     });
   }
@@ -36,6 +36,32 @@ export class PagoService {
       }
     };
 
-    return this.http.post<any>(this.baseUrl, productoConAtributos, { headers });
+    return this.http.post<any>(this.baseUrl, productoConAtributos, { headers })
+      .pipe(
+        map((respuesta) => {
+          console.log('URL de pago:', respuesta);
+
+          // Guardar la URL actual antes de redirigir
+          const currentUrl = this.router.url;
+
+          // Abrir la URL en una nueva ventana o en un WebView
+          window.open(respuesta.init_point, '_blank');
+
+          // Manipular el historial del navegador para volver al HomeClientesPage
+          this.manipularHistorial();
+
+          return respuesta;
+        }),
+        catchError((error) => {
+          console.error('Error al obtener la URL de pago:', error);
+          return throwError(error);
+        })
+      );
+  }
+
+  private manipularHistorial() {
+    // Manipular el historial del navegador para volver al HomeClientesPage
+    const currentUrl = this.router.url;
+    window.history.pushState(null, '', currentUrl);
   }
 }
